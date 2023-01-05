185bdf51576fe44acfeaa9afc61fe27f1c33031e
Juraj Martinka <jumarko@gmail.com>
2023-01-05 09:19:13 +0100

    Update dependencies

--- project.clj
+++ project.clj
@@ -3,10 +3,11 @@
   :url "https://github.com/jumarko/slack-pom"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
-                 [org.julienxx/clj-slack "0.5.5"]
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [org.julienxx/clj-slack "0.6.3"]
                  [overtone/at-at "1.2.0"]
-                 [com.github.tulskiy/jkeymaster "1.2"]]
+                 [com.github.tulskiy/jkeymaster "1.3"]]
   :main ^:skip-aot slack-pom.core
+  :manifest {"git-commit-sha" ~(fn [_project] (:out (clojure.java.shell/sh "git" "rev-list" "head" "-1")))}
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
